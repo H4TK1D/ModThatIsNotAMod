af0e45b758bffb4f91c0e9a1d13dd2bc6f891aaa
H4TK1D <noahthecube1234@gmail.com>
2022-11-09 22:15:43 +0100

    bucket formatting

--- src/main/java/com/H4T_K1D/modthatisnotamodfortweakingpurposes/items/buckets.java
+++ src/main/java/com/H4T_K1D/modthatisnotamodfortweakingpurposes/items/buckets.java
@@ -10,17 +10,16 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class buckets {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-            "modthatisnotamodfortweakingpurposes");
+        public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
+                        "modthatisnotamodfortweakingpurposes");
 
-    public static final RegistryObject<Item> MOLTEN_ANDESITE_BUCKET = ITEMS.register("molten_andesite_bucket",
-            () -> new BucketItem(FluidsMod.ANDESITE_FLUID, new Item.Properties().stacksTo(1)));
-    public static final RegistryObject<Item> MOLTEN_ANDESITE_ALLOY_BUCKET = ITEMS.register(
-            "molten_andesite_alloy_bucket",
-            () -> new BucketItem(FluidsMod.ANDESITE_ALLOY_FLUID, new Item.Properties().stacksTo(1)));
+        public static final RegistryObject<Item> MOLTEN_ANDESITE_BUCKET = ITEMS.register("molten_andesite_bucket",
+                        () -> new BucketItem(FluidsMod.ANDESITE_FLUID, new Item.Properties().stacksTo(1)));
+        public static final RegistryObject<Item> MOLTEN_ANDESITE_ALLOY_BUCKET = ITEMS.register(
+                        "molten_andesite_alloy_bucket",
+                        () -> new BucketItem(FluidsMod.ANDESITE_ALLOY_FLUID, new Item.Properties().stacksTo(1)));
 
-    // ok
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+        public static void register(IEventBus eventBus) {
+                ITEMS.register(eventBus);
+        }
 }
